6cafcb09139a7d809d5f4e277d88582b9a7c1f8b
valentin pitel <valentin.pitel@live.fr>
2016-11-22 17:36:53 +0100

    test1

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,5 @@
-// a simple generator for random adjective-noun-composed names
+
+
 var nameGenerator = (function() {
   var nouns = ['Circle', 'Cone', 'Cylinder', 'Ellipse', 'Hexagon', 'Irregular Shape', 'Octagon', 'Oval', 'Parallelogram', 'Pentagon', 'Pyramid', 'Rectangle', 'Semicircle', 'Sphere', 'Square', 'Star', 'Trapezoid', 'Triangle', 'Wedge', 'Whorl'];
   var adjectives = ['Amusing', 'Athletic', 'Beautiful', 'Brave', 'Careless', 'Clever', 'Crafty', 'Creative', 'Cute', 'Dependable', 'Energetic', 'Famous', 'Friendly', 'Graceful', 'Helpful', 'Humble', 'Inconsiderate', 'Likable', 'Middle Class', 'Outgoing', 'Poor', 'Practical', 'Rich', 'Sad', 'Skinny', 'Successful', 'Thin', 'Ugly', 'Wealth'];
@@ -25,17 +26,19 @@ if (def(wsname)) {
   wsname = nameGenerator();
   document.cookie = "wsname=" + encodeURIComponent(wsname);
 }
-document.querySelector('header>p').textContent = decodeURIComponent(wsname);
+document.querySelector('header>p').textContent = "Reception capteur de "+decodeURIComponent(wsname);
 
 
 
 // create a WebSocket to the server
-var ws = new WebSocket("ws://" + window.location.host);//new WebSocket("ws://127.0.0.1:8080/WSServer_war_exploded/websocket/nope");
-
+//var ws = new WebSocket("ws://" + window.location.host);//new WebSocket("ws://127.0.0.1:8080/WSServer_war_exploded/websocket/nope");
+/*
+var ws = new WebSocket("ws://127.0.0.1:8080");
 
 // we get notified once connected to the server
 ws.onopen = function(event) {
   console.log("We are connected.");
+
 };
 
 // listen to messages comming from the server. When it happens, create a new <li> and append it to the DOM.
@@ -53,12 +56,65 @@ var sendInput = document.querySelector('form input');
 sendForm.addEventListener('submit', sendMessage, true);
 sendForm.addEventListener('blur', sendMessage, true);
 
+
 function sendMessage(event) {
   event.preventDefault();
   event.stopPropagation();
   if (sendInput.value !== '') {
     // send data through the WebSocket
-    ws.send(sendInput.value);
+    ws.send(sendInput.value+'lol');
     sendInput.value = '';
   }
 }
+*/
+var client = new Paho.MQTT.Client("127.0.0.1", Number(8080), "clientId" + Date.now().toString());
+// set callback handlers
+var sensorTest;
+client.onConnectionLost = function (responseObject) {
+  console.log("Connection Lost: "+responseObject.errorMessage);
+}
+
+client.onMessageArrived = function (message) {
+  line = document.createElement('li');
+/*
+  line.textContent = JSON.parse(message.payloadString).type+" - "+
+      message.destinationName.substr(message.destinationName.indexOf("/")+1,
+          message.destinationName.length)+"  ";//+ sensorTest.toString();
+*/
+  var id = message.destinationName.substr(message.destinationName.indexOf("/")+1,
+      message.destinationName.length);
+  var stringJson= {
+    "id": id,
+    "name": id,
+    "type": JSON.parse(message.payloadString).type,
+    "data": {
+      "value": JSON.parse(message.payloadString).value
+    }
+  };
+  sensorTest = TypeSensor(stringJson);
+
+  line.textContent= "name : "+sensorTest.name +"\r\n";
+  line.textContent+="id : "+sensorTest.id+"\r\n";
+  line.textContent+="value : "+sensorTest.data.value+"  (type:"+stringJson.type+")";
+
+  messages.appendChild(line);
+  line.innerHTML = line.innerHTML.replace(/\n\r?/g, '<br />');
+ // console.log("Message Arrived: "+message.payloadString.toString());
+}
+
+// Called when the connection is made
+function onConnect(){
+  console.log("Connected!");
+  client.subscribe('value/#', {qos: 0});
+
+}
+function failure(){
+  console.log("Connected!");
+
+}
+
+// Connect the client, providing an onConnect callback
+client.connect({
+  onSuccess: onConnect,
+  onFailure: failure
+});
